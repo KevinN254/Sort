98a6af7abc5c9fb6bb04acf2ce27f7846f0c7e3a
Ngunjiri <kevin.ngunjiri@autotrader.com>
2016-09-27 21:29:57 -0400

    QuickSort Function Added

--- sort.js
+++ sort.js
@@ -6,34 +6,60 @@ $(document).ready(function(){
 		// get the values from the input field
 		var input = $("#userInput").val();
 		var numbers = (input.split(','));
+		//making sure everything on the inout field is a number
+
 		var len = numbers.length;
 		//Turning the array from string to numbers
 		for (var i = 0; i < len; i++)
 		{
 			var number = parseInt(numbers[i]);
+
+			if(numbers[i]===NaN)
+			{
+				printResults("Make sure you have numbers only, and no extra spaces or commas");
+			}
 			numbers[i] = number;
+		
 		}
 		printResults("Numbers before Sort "+ numbers);
 		var input = $("#userInput").val("");
 		
 
 		//check which radio button is selected using if statement to call function selected
+		// $("input[name=userChoice]:radio").click(function () {
+  //       if ($('input[name=userChoice]:checked').val() == "bubbleSortRadio") {
+  //           	bubbleSort(numbers);
+		// 		printResults("Numbers After BubbleSort " + numbers);
+
+  //       }
+  //       else if ($('input[name=userChoice]:checked').val() == "quickSortRadio") {
+	 //            quickSort(numbers);
+		// 		printResults("Numbers After QuickSort " + numbers);
+
+  //       }
+  //       else
+		// 	{
+		// 		printResults("You need to select what type of sort you want to do");
+		// 	}
+		//     });
+		// });
 		if(($("#bubbleSortRadio").prop("checked"))===true)
 			{
 				bubbleSort(numbers);
 				printResults("Numbers After BubbleSort " + numbers);
 			 }
 
-		else if(($("quickSortRadio").prop("checked"))===true)
+		else(($("quickSortRadio").prop("checked"))===true)
 			{	
 				// swap(numbers);
 				quickSort(numbers);
 				printResults("Numbers After QuickSort " + numbers);
 			}
-		else
-			{
-				printResults("You need to select what type of sort you want to do");
-			}
+		//I was trying to see if I can hinder a user from not clicking any radio button, dint work out
+		// else
+		// 	{
+		// 		printResults("You need to select what type of sort you want to do");
+		// 	}
 		 		
 		//Showing the results in the HTML
 		function printResults(results)
@@ -42,13 +68,13 @@ $(document).ready(function(){
 				$("#resultsDisplay").append(print);
 			}
 
-		//BubbleSort Function
+		//Swap Function
 		function swap(numbers, firstIndex, secondIndex){
 		    var temp = numbers[firstIndex];
 		    numbers[firstIndex] = numbers[secondIndex];
 		    numbers[secondIndex] = temp;
 		}
-
+		//BubbleSort Function
 		function bubbleSort(numbers)
 		{
 
@@ -65,23 +91,23 @@ $(document).ready(function(){
 			}
 		    return numbers;
 		}
-		
+		//QuickSort Function
 		function quickSort(numbers, left, right) {
 		  left = left || 0;
 		  right = right || len - 1;
 
-		  var pivot = partitionHoare(numbers, left, right);
+		  var pivot = partition(numbers, left, right);
 
 		   if(left < pivot - 1) {
-			    quicksort(numbers, left, pivot - 1);
+			    quickSort(numbers, left, pivot - 1);
 			  }
 			  if(right > pivot) {
-			    quicksort(numbers, pivot, right);
+			    quickSort(numbers, pivot, right);
 			  }
 			  return numbers;
 		}
-
-		function partitionHoare(numbers, left, right) {
+		//Partition for quicksort 
+		function partition(numbers, left, right) {
 		  var pivot = Math.floor((left + right) / 2 );
 
 		  while(left <= right) {
@@ -100,7 +126,7 @@ $(document).ready(function(){
 		  return left;
 		}
 
-		console.log(quickSort(numbers.slice()));
+		// console.log(quickSort(numbers.slice()));
 
 		
 	});
